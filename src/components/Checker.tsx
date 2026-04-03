c15e81f107111c91e928a7d0c8582fc38f1be44d
gggosle <gakhovalera2004@gmail.com>
2026-04-03 11:10:09 +0300

    Fix the z-index in undo animation.

--- src/components/Checker.tsx
+++ src/components/Checker.tsx
@@ -1,6 +1,6 @@
-import React, {memo} from 'react';
+import React, { memo, useState, useEffect, useRef } from 'react';
 import { CSS_BOARD } from '../constants.ts';
-import {Color, type Checker as CheckerType} from "../types/game.ts";
+import { Color, type Checker as CheckerType } from "../types/game.ts";
 
 interface CheckerProps {
     piece: CheckerType;
@@ -9,6 +9,20 @@ interface CheckerProps {
 }
 
 export const Checker: React.FC<CheckerProps> = memo(({ piece, isSelected, onClick }) => {
+    const [isAnimating, setIsAnimating] = useState(false);
+
+    const prevPos = useRef({ row: piece.row, col: piece.col });
+
+    useEffect(() => {
+        if (prevPos.current.row !== piece.row || prevPos.current.col !== piece.col) {
+            setIsAnimating(true);
+            prevPos.current = { row: piece.row, col: piece.col };
+            const timer = setTimeout(() => setIsAnimating(false), 300);
+
+            return () => clearTimeout(timer);
+        }
+    }, [piece.row, piece.col]);
+
     const classNames = [
         CSS_BOARD.CHECKER_CLASS,
         piece.color === Color.WHITE ? CSS_BOARD.PLAYER_1_CHECKER_CLASS : CSS_BOARD.PLAYER_2_CHECKER_CLASS,
@@ -17,13 +31,14 @@ export const Checker: React.FC<CheckerProps> = memo(({ piece, isSelected, onClic
     ].filter(Boolean).join(' ');
 
     const offset = `calc((var(--cell-size) - var(--checker-size)) / 2)`;
+    const currentZIndex = isAnimating ? 50 : 10;
 
     const style: React.CSSProperties = {
         position: 'absolute',
         top: `calc(${piece.row} * var(--cell-size) + ${offset})`,
         left: `calc(${piece.col} * var(--cell-size) + ${offset})`,
         transition: 'top var(--transition-normal), left var(--transition-normal), transform var(--transition-fast)',
-        zIndex: isSelected ? 100 : 10,
+        zIndex: currentZIndex,
         margin: 0
     };
 
@@ -37,4 +52,4 @@ export const Checker: React.FC<CheckerProps> = memo(({ piece, isSelected, onClic
             }}
         />
     );
-})
+});
